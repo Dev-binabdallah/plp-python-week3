c13bf562432e86087755e40295e20ea36a6cafd0
Dev-binabdallah <bin.abdallah.jr@gmail.com>
2026-08-15 08:40:49 +0300

    add my python greeter

---
+++ name_greeter.py
@@ -0,0 +1,10 @@
+full_name = str(input("Enter your full name: "))
+names = full_name.split()
+first = names[0]
+second = names[-1]
+if  len(names) >= 2:
+    print("Good morning:", first)
+if len(names) == 1:
+    print("Please write your full name: ")
+    
+     
